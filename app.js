ba03943ad2cb78bc1faf57044b216e94f0130059
RH108 <128111387+RH108@users.noreply.github.com>
2024-08-11 20:33:25 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,160 @@
+const express = require("express");
+const ejs = require("ejs");
+const engine = require("ejs-mate");
+const path = require("path");
+const http = require("http");
+const socketIO = require("socket.io");
+const { Issuer, TokenSet, custom, generators } = require("openid-client");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const clientId = "5139357317539084266";
+const clientSecret = "RBX-643fLjgdskCes590j8qzxUKIFPNHdK6HI7OLDJ2IB0ywYBbdP2VN3HLKgd_a3o4l";
+const cookieSecret = process.env.COOKIE_SECRET || generators.random();
+const secureCookieConfig = {
+  secure: false,
+  httpOnly: true,
+  signed: true,
+};
+
+app.engine("ejs", engine);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public", {
+  setHeaders: (res, path) => {
+    if (path.endsWith(".js")) {
+      res.setHeader("Content-Type", "application/javascript");
+    }
+  }
+}));
+
+app.use(cookieParser(cookieSecret));
+app.use(express.urlencoded({ extended: true }));
+
+async function main() {
+  const issuer = await Issuer.discover(
+    "https://apis.roblox.com/oauth/.well-known/openid-configuration"
+  );
+
+  const client = new issuer.Client({
+    client_id: clientId,
+    client_secret: clientSecret,
+    redirect_uris: [`http://localhost:3000/oauth/callback`],
+    response_types: ["code"],
+    scope: "openid profile",
+    id_token_signed_response_alg: "ES256",
+  });
+
+  client[custom.clock_tolerance] = 180;
+
+  async function checkLoggedIn(req, res, next) {
+    if (req.signedCookies.tokenSet) {
+      let tokenSet = new TokenSet(req.signedCookies.tokenSet);
+
+      if (tokenSet.expired()) {
+        try {
+          tokenSet = await client.refresh(tokenSet);
+          res.cookie("tokenSet", tokenSet, secureCookieConfig);
+        } catch (error) {
+          console.error('Error refreshing token:', error);
+          return res.redirect('/login');
+        }
+      }
+
+      next();
+    } else {
+      res.redirect("/login");
+    }
+  }
+
+  app.get("/", checkLoggedIn, (req, res) => {
+    res.redirect("/rowager");
+  });
+
+  app.get("/login", (req, res) => {
+    const state = generators.state();
+    const nonce = generators.nonce();
+
+    res
+      .cookie("state", state, secureCookieConfig)
+      .cookie("nonce", nonce, secureCookieConfig)
+      .redirect(
+        client.authorizationUrl({
+          scope: client.scope,
+          state,
+          nonce,
+        })
+      );
+  });
+
+  app.get("/logout", async (req, res) => {
+    if (req.signedCookies.tokenSet) {
+      client.revoke(req.signedCookies.tokenSet.refresh_token);
+    }
+
+    res.clearCookie("tokenSet").redirect("/");
+  });
+
+  app.get("/oauth/callback", async (req, res) => {
+    try {
+      const params = client.callbackParams(req);
+      const tokenSet = await client.callback(
+        `http://localhost:3000/oauth/callback`,
+        params,
+        {
+          state: req.signedCookies.state,
+          nonce: req.signedCookies.nonce,
+        }
+      );
+
+      res
+        .cookie("tokenSet", tokenSet, secureCookieConfig)
+        .clearCookie("state")
+        .clearCookie("nonce")
+        .redirect("/rowager");
+    } catch (error) {
+      console.error('Error handling OAuth callback:', error);
+      res.redirect('/login');
+    }
+  });
+
+  var placeHolder
+
+  app.get("/rowager", checkLoggedIn, (req, res) => {
+    const tokenSet = new TokenSet(req.signedCookies.tokenSet);
+
+    io.emit('login_client', tokenSet.claims());
+    placeHolder = tokenSet.claims()
+    console.log('Emitting login_client event with token:', tokenSet.claims());
+
+    res.render("home.ejs");
+  });
+
+  io.on("connection", (socket) => {
+    console.log('A user connected');
+
+    socket.on("message", (user, msg) => {
+      console.log(user + ": " + msg);
+      io.emit("newtext", user, msg);
+    });
+
+    socket.on('login_request', () => {
+      socket.emit('login_return', placeHolder)
+    });
+
+    socket.on('disconnect', () => {
+      console.log('User disconnected');
+    });
+  });
+
+  server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+}
+
+main().catch(console.error);
